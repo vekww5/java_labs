9e5145907898ead728f7974fbc630de43872b81b
Kuznetsov Vadim <90772765+vekww5@users.noreply.github.com>
2021-10-18 05:34:53 +0300

    Add files via upload

---
+++ lab2/my-app-test/src/main/java/com/print/PrintClass.java
@@ -0,0 +1,7 @@
+package com.print;
+
+public class PrintClass{
+	public void printText(String stroka){
+		System.out.println(stroka);
+	}
+}
---
+++ lab2/my-app2/src/main/java/com/mycompany/lab21/ExempPrint.java
@@ -0,0 +1,11 @@
+package com.mycompany.lab21;
+
+import com.print.PrintClass;
+
+public class ExempPrint{
+
+	public static void main(String[] args) {
+		PrintClass pc = new PrintClass();
+		pc.printText("Text");
+	}
+} 
